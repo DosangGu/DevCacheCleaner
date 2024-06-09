ab56c9452f5ff9c42c2a4e32ee096555b27fb8c2
DosangGu <ddos0418@gmail.com>
2024-06-09 17:01:36 +0900

    feat: add NugetPathFinder

---
+++ DevCacheCleaner/NugetPathFinder.cs
@@ -0,0 +1,21 @@
+﻿namespace DevCacheCleaner;
+
+internal static class NugetPathFinder
+{
+    public static string GetNugetPackagesPath()
+    {
+        string? customNugetPath = Environment.GetEnvironmentVariable("NUGET_PACKAGES");
+        if (customNugetPath is not null)
+        {
+            return customNugetPath;
+        }
+        else
+        {
+            return Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                ".nuget",
+                "packages"
+            );
+        }
+    }
+}
