7f52a70a887559c590f2c052e8c4ba30c99fbaee
DosangGu <ddos0418@gmail.com>
2024-06-13 21:25:58 +0900

    feat: Add project class

---
+++ src/DevCacheCleaner/Build/Project.cs
@@ -0,0 +1,44 @@
+﻿using DevCacheCleaner.Shared;
+
+namespace DevCacheCleaner.Build;
+
+internal class Project
+{
+    private string? _binPath;
+    private string? _objPath;
+
+
+    public string Path { get; }
+
+    public Project(string path)
+    {
+        this.Path = path;
+    }
+
+    public void FindBuildArtifacts()
+    {
+        this._binPath = System.IO.Path.Combine(this.Path, "bin");
+        this._objPath = System.IO.Path.Combine(this.Path, "obj");
+    }
+
+    public void CleanBuildArtifacts()
+    {
+        if (FileSystemHelper.IsFileOfDirectoryInUse(this._binPath!))
+        {
+            Console.WriteLine($"Skip deleting {this._binPath} because it is in use by another process.");
+        }
+        else
+        {
+            Directory.Delete(this._binPath!, true);
+        }
+
+        if (FileSystemHelper.IsFileOfDirectoryInUse(this._objPath!))
+        {
+            Console.WriteLine($"Skip deleting {this._objPath} because it is in use by another process.");
+        }
+        else
+        {
+            Directory.Delete(this._objPath!, true);
+        }
+    }
+}
