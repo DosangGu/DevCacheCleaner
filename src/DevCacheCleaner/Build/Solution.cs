9a9b9550cfe4e0096960b5ec2f5b5be21937c933
DosangGu <ddos0418@gmail.com>
2024-06-13 21:40:51 +0900

    feat: Add Solution class

---
+++ src/DevCacheCleaner/Build/Solution.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Build.Construction;
+
+namespace DevCacheCleaner.Build;
+
+internal class Solution
+{
+    private readonly List<Project> _projects;
+
+    public string Path { get; }
+
+    public IReadOnlyList<Project> Projects => this._projects;
+
+    public Solution(string path)
+    {
+        this.Path = path;
+        this._projects = [];
+    }
+
+    public void FindProjects()
+    {
+        var solutionFile = SolutionFile.Parse(this.Path);
+        var projects = solutionFile.ProjectsInOrder
+            .Where(p => p.ProjectType == SolutionProjectType.KnownToBeMSBuildFormat)
+            .Select(p => new Project(p.AbsolutePath));
+
+        foreach (var project in projects)
+        {
+            this._projects.Add(project);
+        }
+    }
+
+    public void CleanBuildArtifacts()
+    {
+        foreach (var project in this._projects)
+        {
+            project.FindBuildArtifacts();
+            project.CleanBuildArtifacts();
+        }
+    }
+}
